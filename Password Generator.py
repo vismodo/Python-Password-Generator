c60e9534e8329e71de0bff8b68fd853b27ebf6ec
Vismaya Atreya <62926183+VismayaAtreya@users.noreply.github.com>
2020-08-09 14:29:52 +0530

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter.messagebox import showinfo
 import random as r
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-more = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '&', '*', '/', '?', '.']
+more = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '&', '*', '/', '?', '.']
 start = tk.Tk()
 start.title('Python Password Generator')
 start.geometry('500x100')
